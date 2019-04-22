9d043b610e0a99dca2dcfba7dd6140222402c080
Prince Odzalas <princeodzalasapp@gmail.com>
2019-04-22 22:36:31 +0200

    WM Library 0.0.6

--- wmlibraryandroidx/src/main/java/com/princeodzalasapp/fr/wmlibraryandroidx/Themes/Themes.java
+++ wmlibraryandroidx/src/main/java/com/princeodzalasapp/fr/wmlibraryandroidx/Themes/Themes.java
@@ -1,15 +1,19 @@
 package com.princeodzalasapp.fr.wmlibraryandroidx.Themes;
 
 import android.app.Activity;
-import android.content.SharedPreferences;
-
-import static android.content.Context.MODE_PRIVATE;
-import static com.princeodzalasapp.fr.wmlibraryandroidx.Services.Constantes.KEY_PREFS;
-import static com.princeodzalasapp.fr.wmlibraryandroidx.Services.Constantes.KEY_THEME;
 
 public class Themes {
 
-    private static SharedPreferences prefs;
+    public static void set(int primaryColor, int accentColor,
+                                boolean translucent, boolean dark, Activity act) {
+        ThemeColor.applyTheme(act);
+        ThemeColor.config(act)
+                .primaryColor(getThemeColor(primaryColor))
+                .accentColor(getThemeColor(accentColor))
+                .translucent(translucent)
+                .dark(dark)
+                .apply();
+    }
 
 
     /**
@@ -84,8 +88,31 @@ public class Themes {
         setColor(primaryColor, accentColor, false, true, act);
     }
 
-    public static void getThemes() {
-
+    public static ThemeColor.ThemeColors getThemeColor(int val){
+        switch(val){
+            case 1 : return ThemeColor.ThemeColors.RED;
+            case 2 : return ThemeColor.ThemeColors.PINK;
+            case 3 : return ThemeColor.ThemeColors.PURPLE;
+            case 4 : return ThemeColor.ThemeColors.DEEP_PURPLE;
+            case 5 : return ThemeColor.ThemeColors.INDIGO;
+            case 6 : return ThemeColor.ThemeColors.BLUE;
+            case 7 : return ThemeColor.ThemeColors.LIGHT_BLUE;
+            case 8 : return ThemeColor.ThemeColors.CYAN;
+            case 9 : return ThemeColor.ThemeColors.TEAL;
+            case 10 : return ThemeColor.ThemeColors.GREEN;
+            case 11 : return ThemeColor.ThemeColors.LIGHT_GREEN;
+            case 12 : return ThemeColor.ThemeColors.LIME;
+            case 13 : return ThemeColor.ThemeColors.YELLOW;
+            case 14 : return ThemeColor.ThemeColors.AMBER;
+            case 15 : return ThemeColor.ThemeColors.ORANGE;
+            case 16 : return ThemeColor.ThemeColors.DEEP_ORANGE;
+            case 17 : return ThemeColor.ThemeColors.BROWN;
+            case 18 : return ThemeColor.ThemeColors.GREY;
+            case 19 : return ThemeColor.ThemeColors.BLUE_GREY;
+            case 20 : return ThemeColor.ThemeColors.WHITE;
+            case 21 : return ThemeColor.ThemeColors.BLACK;
+            default: return ThemeColor.ThemeColors.RED;
+        }
     }
 
 }
